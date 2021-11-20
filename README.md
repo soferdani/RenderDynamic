b5fb42be4e381f345c2cde6a713782499e916f6a
soferdani <soferdani@gmail.com>
2021-11-20 22:25:36 +0200

    that it for today

--- README.md
+++ README.md
@@ -3,21 +3,7 @@
 This project was created by Dani sofer as a home assignment.
 
 ## Description
-This application display board of notes and allows the user the add notes as well. 
-
-## Main features 
-* The board notes stored in the local storage and let you refresh the page and still receive your notes.
-* When click on one of the notes a model is open and display its content 
-* When click on the new post card a model is open with inputs area 
-* You must provide content in both the content input area and author input area in order to successfully submit your note. 
-
-## Technologies
-* I use React as the main user interface building blacks with help of -
-*  React-Bootstrap.
-* I use nanoid for creation of the id's of the notes. 
-
-
-## Photos
+This application render a dynamic components from JSON file provided. 
 
 ## Installation
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app)
--- src/components/TabAreaComponent.js
+++ src/components/TabAreaComponent.js
@@ -1,36 +1,42 @@
 import { renderComponent } from "../render/renderComponent";
 import { useState } from "react";
 
+export default function TabAreaComponent({ config }) {
+	const [activeTab, setActiveTab] = useState(null);
 
+	const generalTabHandler = (tabName) => {
+		setActiveTab(tabName);
+	};
 
-export default function TabAreaComponent({config}) {
-    const [activeTab, setActiveTab] = useState(null)
-
-    const generalTabHandler = (tabName) => {
-        setActiveTab(tabName)
-    }
-
+    let componentArray = [];
 
     return (
-        <div id={config.id} className='tab-area'>
-            {config.children.map((child, index) => {
-                console.log(child.components);
-
-                if (child.components.length > 0) {
-                    
-                }
-
-                    return (
-                        <div key={index}>
-                            {child.title}
+        <>
+		<div id={config.id} className='tab-area'>
+			{config.children.map((child, index) => {
+				return (
+					<>
+						<ul key={index}>
+							<li onClick={() => generalTabHandler(child.title)}>
+								{child.title}
+							</li>
+						</ul>
+						<div className='outlet'>
                             {child.components.map((oneChild) => {
-                                return (
-                                    renderComponent(oneChild)
-                                )
-                            })}
-                        </div>
-                    )
-            })}
-        </div>
-    );
+                                componentArray.push(renderComponent(oneChild))
+								{/* return renderComponent(oneChild); */}
+							})}
+						</div>
+					</>
+				);
+			})}
+            </div>
+            <div className='outlet'>
+                {activeTab && <h2>{activeTab}</h2>}
+                {componentArray}
+            </div>
+        </>
+
+
+	);
 }
