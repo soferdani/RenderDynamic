8ae8c5a51dfa11c6bef5a45cfb149adc0f6e22eb
soferdani <soferdani@gmail.com>
2021-11-21 19:23:41 +0200

    finel fix

--- src/components/LabelComponent.js
+++ src/components/LabelComponent.js
@@ -3,8 +3,14 @@ import { useSelector } from "react-redux";
 export default function LabelComponent({ config }) {
     let UserId = useSelector(state => state);
     return (
-        <label id={config.id} >
-            {UserId.userInput.inputToDisplay}
-        </label>
+        <>
+            <div>
+                <br/>
+                <label id={config.id} >
+                {UserId.userInput.inputToDisplay}
+                </label>
+            </div>
+            
+        </>
     );
 }
